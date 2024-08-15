3a3978bbfb7aba24e6d984f6b0f32f383c6823a0
kasturi2903 <pawarkasturi2906@gmail.com>
2024-08-15 22:13:22 +0530

    chat component updated

--- client/src/compoenents/chat/chat.jsx
+++ client/src/compoenents/chat/chat.jsx
@@ -1,47 +1,56 @@
-import React, { useState } from 'react'
-import "./chat.scss"
+import React, { useState } from 'react';
+import "./chat.scss";
+
 function Chat() {
     const [chat, setChat] = useState(true);
+
+    const toggleChat = () => {
+        setChat(prevChat => !prevChat); // Toggles between true and false
+    };
+
     return (
         <div className='chat'>
             <div className="messages">
                 <h1>Messages</h1>
-                <div className="message">
+                <div className="message" onClick={toggleChat}>
                     <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                     <span>John Doe</span>
                     <p>Lorem, ipsum dolor sit amet consectetur...</p>
                 </div>
-                <div className="message">
+                <div className="message" onClick={toggleChat}>
+                    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <span>Jade</span>
+                    <p>Lorem, ipsum dolor sit amet consectetur...</p>
+                </div>
+                <div className="message" onClick={toggleChat}>
                     <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                     <span>John Doe</span>
                     <p>Lorem, ipsum dolor sit amet consectetur...</p>
                 </div>
-                <div className="message">
+                <div className="message" onClick={toggleChat}>
                     <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                     <span>John Doe</span>
                     <p>Lorem, ipsum dolor sit amet consectetur...</p>
                 </div>
-                <div className="message">
+                <div className="message" onClick={toggleChat}>
                     <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                     <span>John Doe</span>
                     <p>Lorem, ipsum dolor sit amet consectetur...</p>
                 </div>
-                <div className="message">
+                <div className="message" onClick={toggleChat}>
                     <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                     <span>John Doe</span>
                     <p>Lorem, ipsum dolor sit amet consectetur...</p>
                 </div>
             </div>
             {chat && (
-                (<div className="chat-box">
+                <div className="chat-box">
                     <div className="top">
                         <div className="user">
                             <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                             John Doe
                         </div>
-                        <span className='close' onClick={() => {
-                            setChat(null);
-                        }}>X</span>
+                        <span className='close' onClick={toggleChat}>X</span>
                     </div>
                     <div className="center">
                         <div className="chatmessage own">
@@ -69,9 +78,10 @@ function Chat() {
                         <textarea></textarea>
                         <button>Send</button>
                     </div>
-                </div>))}
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default Chat;
